624840b7baac6901f8ea17d657727d312cce7847
Philip C. Okoh <pco23@man.cs.rutgers.edu>
2022-04-06 12:44:15 -0400

    Just started

--- stopandwait.py
+++ stopandwait.py
@@ -170,15 +170,20 @@ def send_reliable(cs, filedata, receiver_binding, win_size):
 
     # TODO: This is where you will make your changes. You
     # will not need to change any other parts of this file.
-
+    first_to_tx = int
     while win_left_edge < INIT_SEQNO + content_len:
-        bytes_sent = transmit_one()
+        bytes_sent = transmit_entire_window_from(win_left_edge)
 
         # Use select to see if we can read from a socket.
         read_sock, write_sock, err_sock = select.select([cs], [], [], RTO)
-
+        if not (read_sock, write_sock, err_sock):
+            transmit_one()
+            continue
         if cs in read_sock:
             try:
+                
+                
+                
                 data_from_receiver, receiver_addr = cs.recvfrom(100)
                 ack_msg = Msg.deserialize(data_from_receiver)
                 print("Received {}".format(ack_msg))
@@ -188,10 +193,10 @@ def send_reliable(cs, filedata, receiver_binding, win_size):
                 if bytes_ack == bytes_sent:
                     win_left_edge = bytes_ack
 
-
             except socket.error:
                 # If the receiver socket isn't open, we wait for a bit and try again.
                 time.sleep(RTO * 3)
+            
 
 if __name__ == "__main__":
     args = parse_args()
